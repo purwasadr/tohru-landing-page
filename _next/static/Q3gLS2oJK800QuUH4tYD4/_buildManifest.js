ffb3d5e8d4f1ae7a8dbc01611d88cee3cb773dce
Purwa Sadr <purwasadr@gmail.com>
2023-02-25 09:11:58 +0700

    Updates

---
+++ _next/static/Q3gLS2oJK800QuUH4tYD4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/163-7e0fcdc25c9e3450.js","static/chunks/pages/index-dcab2b8baf9c010c.js"],"/_error":["static/chunks/pages/_error-2c2a9bcd4124449a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
